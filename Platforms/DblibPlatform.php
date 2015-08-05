cce3041d461080d67eb69df3c2052c42243e39af
Cindy Paulitz <cindy@dreiwerken.de>
2015-08-05 15:11:25 +0200

    first commit

---
+++ Platforms/DblibPlatform.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Realestate\MssqlBundle\Platforms;
+
+use Doctrine\DBAL\Platforms\SQLServer2008Platform;
+
+/**
+ * The DblibPlatform provides the behavior, features and SQL dialect of the
+ * MsSQL database platform.
+ */
+class DblibPlatform extends SQLServer2008Platform
+{
+    /**
+     * Get the platform name for this instance
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'mssql';
+    }
+
+   /**
+     * {@inheritDoc}
+     */
+    public function supportsSchemas()
+    {
+        return true;
+    }    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getListTablesSQL()
+    {
+        // "sysdiagrams" table must be ignored as it's internal SQL Server table for Database Diagrams
+        // Category 2 must be ignored as it is "MS SQL Server 'pseudo-system' object[s]" for replication
+        return "SELECT scm.name+'.'+obj.name as name FROM sys.tables as obj JOIN sys.schemas as scm on obj.schema_id = scm.schema_id order by obj.name";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDateTimeTypeDeclarationSQL(array $fieldDeclaration)
+    {
+        return 'DATETIME';
+    }
+
+}
